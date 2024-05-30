390689df8701875aac9485119137e6f4d400cc66
harlanfadhilah69 <harlancoegg@gmail.com>
2024-05-30 18:46:49 +0700

    case 1,2,3,4

--- circularQueuee/circularQueuee.cpp
+++ circularQueuee/circularQueuee.cpp
@@ -105,4 +105,21 @@ int main() {
 			cin >> ch;
 			cout << endl;
 
+			switch (ch) {
+			case '1': {
+
+				q.insert();
+				break;
+			}
+			case '2': {
+				q.remove();
+				break;
+			}
+			case '3': {
+				q.display();
+				break;
+			}
+			case '4': {
+				return 0;
+			}
 		}
