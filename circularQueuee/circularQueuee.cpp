e9e76a542bfc7ec9ce316a4776b71a111467b396
harlanfadhilah69 <harlancoegg@gmail.com>
2024-05-30 18:34:00 +0700

    iterasi dari awal array hingga rear

--- circularQueuee/circularQueuee.cpp
+++ circularQueuee/circularQueuee.cpp
@@ -73,6 +73,19 @@ public:
 			}
 			cout << endl;
 		}
-	}
+		else {
+			while (FRONT_position <= max - 1) {
+				cout << queue_array[FRONT_position] << " ";
+				FRONT_position++;
+			}
 
+			FRONT_position = 0;
+
+			while (FRONT_position <= REAR_position) {
+				cout << queue_array[FRONT_position] << " ";
+				FRONT_position;
+			}
+			cout << endl;
+		}
+	}
 };
