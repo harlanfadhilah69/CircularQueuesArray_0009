3aca451027ce8539d8f9b7f94037fc9cc4baf545
harlanfadhilah69 <harlancoegg@gmail.com>
2024-05-30 18:50:16 +0700

    default catch return

--- circularQueuee/circularQueuee.cpp
+++ circularQueuee/circularQueuee.cpp
@@ -122,4 +122,13 @@ int main() {
 			case '4': {
 				return 0;
 			}
+			default: {
+				cout << "Invalid option!!" << endl;
+			}
+			}
+		}
+		catch (exception& e) {
+			cout << "Check for the values entered." << endl;
+		}
+			return 0;
 		}
